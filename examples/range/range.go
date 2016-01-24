13c1a610ebcc44295b68423cee9f30bca027070d
Howl <git@howl.moe>
2016-01-24 11:33:41 +0100

    Localisation for example 11: range

--- examples/range/range.go
+++ examples/range/range.go
@@ -1,6 +1,7 @@
-// _range_ iterates over of elements in a variety of
-// data structures. Let's see how to use `range` with some
-// of the data structures we've already learned.
+// I _range_ statement reiterano sugli elementi di una
+// varietà di strutture di dati, similmente ai foreach di
+// altri linguaggi. Vediamo come usare `range` con alcune
+// delle strutture di dati che abbiamo già visto.
 
 package main
 
@@ -8,36 +9,39 @@ import "fmt"
 
 func main() {
 
-    // Here we use `range` to sum the numbers in a slice.
-    // Arrays work like this too.
-    nums := []int{2, 3, 4}
-    sum := 0
-    for _, num := range nums {
-        sum += num
-    }
-    fmt.Println("sum:", sum)
+	// Di seguito utilizziamo un `range` per sommare i
+	// numeri di uno slice. (Con gli array viene fatto
+	// allo stesso modo).
+	nums := []int{2, 3, 4}
+	sum := 0
+	for _, num := range nums {
+		sum += num
+	}
+	fmt.Println("somma:", sum)
 
-    // `range` on arrays and slices provides both the
-    // index and value for each entry. Above we didn't
-    // need the index, so we ignored it with the
-    // blank identifier `_`. Sometimes we actually want
-    // the indexes though.
-    for i, num := range nums {
-        if num == 3 {
-            fmt.Println("index:", i)
-        }
-    }
+	// `range` sugli array e gli slice ritorna sia l'indice
+	// sia il valore di ognuno degli elementi. Prima non
+	// avevamo bisogno di utilizzare l'indice, quindi
+	// l'avevamo ignorato utilizzando il blank identifier
+	// `_`. Qualche volta potremmo avere anche solo bisogno
+	// dell'indice, e ignorare il suo valore.
+	for i, num := range nums {
+		if num == 3 {
+			fmt.Println("indice:", i)
+		}
+	}
 
-    // `range` on map iterates over key/value pairs.
-    kvs := map[string]string{"a": "apple", "b": "banana"}
-    for k, v := range kvs {
-        fmt.Printf("%s -> %s\n", k, v)
-    }
+	// `range` su una map reitera sulle coppie
+	// chiave-valore.
+	kvs := map[string]string{"a": "arancia", "b": "banana"}
+	for k, v := range kvs {
+		fmt.Printf("%s -> %s\n", k, v)
+	}
 
-    // `range` on strings iterates over Unicode code
-    // points. The first value is the starting byte index
-    // of the `rune` and the second the `rune` itself.
-    for i, c := range "go" {
-        fmt.Println(i, c)
-    }
+	// `range` sulle stringhe reitera sugli Unicode code
+	// point. Il primo valore è l'indice del `rune` nella
+	// stringa, il secondo è il `rune` in sé.
+	for i, c := range "go" {
+		fmt.Println(i, c)
+	}
 }
--- examples/range/range.sh
+++ examples/range/range.sh
@@ -1,7 +1,7 @@
-$ go run range.go 
-sum: 9
-index: 1
-a -> apple
+$ go run range.go
+somma: 9
+indice: 1
+a -> arancia
 b -> banana
 0 103
 1 111
