8912f60881d8f0ce42ebfadab7f908bdeffda6fe
Howl <git@howl.moe>
2016-01-24 12:01:06 +0100

    Localisation for example 11: Multiple return values

--- examples/multiple-return-values/multiple-return-values.sh
+++ examples/multiple-return-values/multiple-return-values.sh
@@ -3,5 +3,5 @@ $ go run multiple-return-values.go
 7
 7
 
-# Accepting a variable number of arguments is another nice
-# feature of Go functions; we'll look at this next.
+# Accettare un numero variabile di valori è un'altra
+# feature di go: vedremo come farlo nel prossimo esempio.
