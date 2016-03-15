85e54757a6da7468bc7726fd5c174b3ab28a86bb
Nicola Corti <corti.nico@gmail.com>
2016-03-16 00:13:03 +0100

    examples: localization for example 58: line filters

--- examples/line-filters/line-filters.go
+++ examples/line-filters/line-filters.go
@@ -1,41 +1,43 @@
-// A _line filter_ is a common type of program that reads
-// input on stdin, processes it, and then prints some
-// derived result to stdout. `grep` and `sed` are common
-// line filters.
+// Un _line filter_ è un tipo di programma molto comune
+// che legge l'input da stdin, lo processa, e ne stampa
+// l'output su stdout. `grep` e `sed` sono comuni esempi
+// di _line filter_.
 
-// Here's an example line filter in Go that writes a
-// capitalized version of all input text. You can use this
-// pattern to write your own Go line filters.
+// Ecco un esempio di line filter scritto in Go, che scrive
+// in maiscuolo tutto il testo che riceve ininput. Puoi
+// utilizzare questo scheletro di line filter per realizzare
+// il tuo line filter.
 package main
 
 import (
-    "bufio"
-    "fmt"
-    "os"
-    "strings"
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
 )
 
 func main() {
 
-    // Wrapping the unbuffered `os.Stdin` with a buffered
-    // scanner gives us a convenient `Scan` method that
-    // advances the scanner to the next token; which is
-    // the next line in the default scanner.
-    scanner := bufio.NewScanner(os.Stdin)
+	// Tramite `NewScanner` creiamo un wrapper intorno a
+	// `os.Stdin` (che è non bufferizzato), permettendoci
+	// di invocare la funzione `Scan` per ottenere il
+	// prossimo token (in questo caso la prossima linea).
+	scanner := bufio.NewScanner(os.Stdin)
 
-    for scanner.Scan() {
-        // `Text` returns the current token, here the next line,
-        // from the input.
-        ucl := strings.ToUpper(scanner.Text())
+	for scanner.Scan() {
+		// `Text` ritorna il token corrente, in questo caso
+		// la prossima linea dall'input.
+		ucl := strings.ToUpper(scanner.Text())
 
-        // Write out the uppercased line.
-        fmt.Println(ucl)
-    }
+		// Stampa la linea in maiuscolo.
+		fmt.Println(ucl)
+	}
 
-    // Check for errors during `Scan`. End of file is
-    // expected and not reported by `Scan` as an error.
-    if err := scanner.Err(); err != nil {
-        fmt.Fprintln(os.Stderr, "error:", err)
-        os.Exit(1)
-    }
+	// Controlla eventuali errori durante lo `Scan`.
+	// Un _End of file_ è atteso dallo scanner e non viene
+	// quindi considerato un errore
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "error:", err)
+		os.Exit(1)
+	}
 }
