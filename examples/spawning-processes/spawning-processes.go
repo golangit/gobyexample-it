cac63b65c5840bb7acb1eb575f3d8d72354c273e
Nicola Corti <corti.nico@gmail.com>
2016-03-22 22:28:29 +0100

    examples: localization for example 62: spawning processes

--- examples/spawning-processes/spawning-processes.go
+++ examples/spawning-processes/spawning-processes.go
@@ -1,10 +1,10 @@
-// Sometimes our Go programs need to spawn other, non-Go
-// processes. For example, the syntax highlighting on this
-// site is [implemented](https://github.com/mmcgrana/gobyexample/blob/master/tools/generate.go)
-// by spawning a [`pygmentize`](http://pygments.org/)
-// process from a Go program. Let's look at a few examples
-// of spawning processes from Go.
-
+// Talvolta i programmi Go hanno bisogno di far partire
+// altri processi, che non sono programmi Go. Ad esempio
+// l'highlight della sintassi in questo sito è [realizzata](https://github.com/golangit/gobyexample-it/blob/master/tools/generate.go)
+// facendo partire un processo [`pygmentize`](http://pygments.org/)
+// da un programma Go.
+// Vediamo qualche esempio su come poter eseguire processi
+// arbitrari da Go.
 package main
 
 import "fmt"
@@ -13,59 +13,60 @@ import "os/exec"
 
 func main() {
 
-    // We'll start with a simple command that takes no
-    // arguments or input and just prints something to
-    // stdout. The `exec.Command` helper creates an object
-    // to represent this external process.
-    dateCmd := exec.Command("date")
+	// Inizieremo con un programma semplice, che non ha argomenti
+	// o input ma che stampa solamente sul stdout. La funzione
+	// `exec.Command` è un helper che rappresenta il processo
+	// che andremo ad eseguire
+	dateCmd := exec.Command("date")
 
-    // `.Output` is another helper that handles the common
-    // case of running a command, waiting for it to finish,
-    // and collecting its output. If there were no errors,
-    // `dateOut` will hold bytes with the date info.
-    dateOut, err := dateCmd.Output()
-    if err != nil {
-        panic(err)
-    }
-    fmt.Println("> date")
-    fmt.Println(string(dateOut))
+	// Tramite la funzione `.Output` è possibile eseguire il
+	// processo, attendere che finisca ed ottenere il output.
+	// Se non ci sono stati errori `dateOut` conterrà i byte
+	// della data.
+	dateOut, err := dateCmd.Output()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("> date")
+	fmt.Println(string(dateOut))
 
-    // Next we'll look at a slightly more involved case
-    // where we pipe data to the external process on its
-    // `stdin` and collect the results from its `stdout`.
-    grepCmd := exec.Command("grep", "hello")
+	// Adesso vedremo un esempio leggermente più complesso
+	// dove forniremo alcuni dati sullo stdin di un processo,
+	// e raccoglieremo il suo output sullo stdout.
+	grepCmd := exec.Command("grep", "hello")
 
-    // Here we explicitly grab input/output pipes, start
-    // the process, write some input to it, read the
-    // resulting output, and finally wait for the process
-    // to exit.
-    grepIn, _ := grepCmd.StdinPipe()
-    grepOut, _ := grepCmd.StdoutPipe()
-    grepCmd.Start()
-    grepIn.Write([]byte("hello grep\ngoodbye grep"))
-    grepIn.Close()
-    grepBytes, _ := ioutil.ReadAll(grepOut)
-    grepCmd.Wait()
+	// Qui possiamo vedere come ottenere lo stdin e lo stdout
+	// di un futuro comando, scrivere dei dati sullo stdin,
+	// leggere dei dati dallo stdout e attendere che il processo
+	// termini.
+	grepIn, _ := grepCmd.StdinPipe()
+	grepOut, _ := grepCmd.StdoutPipe()
+	grepCmd.Start()
+	grepIn.Write([]byte("hello grep\ngoodbye grep"))
+	grepIn.Close()
+	grepBytes, _ := ioutil.ReadAll(grepOut)
+	grepCmd.Wait()
 
-    // We ommited error checks in the above example, but
-    // you could use the usual `if err != nil` pattern for
-    // all of them. We also only collect the `StdoutPipe`
-    // results, but you could collect the `StderrPipe` in
-    // exactly the same way.
-    fmt.Println("> grep hello")
-    fmt.Println(string(grepBytes))
+	// Abbiamo tralasciato il controllo degli errori nelle
+	// righe precedenti, ma ricordiamoci che possiamo usare
+	// il classico pattern `if err != nil` per tutti loro.
+	// Ci limitiamo inoltre a leggere dallo `StdoutPipe`,
+	// ma potremmo leggere anche da `StderrPipe` nello stesso
+	// identico modo.
+	fmt.Println("> grep hello")
+	fmt.Println(string(grepBytes))
 
-    // Note that when spawning commands we need to
-    // provide an explicitly delineated command and
-    // argument array, vs. being able to just pass in one
-    // command-line string. If you want to spawn a full
-    // command with a string, you can use `bash`'s `-c`
-    // option:
-    lsCmd := exec.Command("bash", "-c", "ls -a -l -h")
-    lsOut, err := lsCmd.Output()
-    if err != nil {
-        panic(err)
-    }
-    fmt.Println("> ls -a -l -h")
-    fmt.Println(string(lsOut))
+	// Nota che per eseguire processi con flag e parametri
+	// è necessario passare un array che li contenga, non è
+	// possibile passare semplicemente una stringa.
+	// Se si vuole eseguire un processo passando la sua stringa
+	// è possibile utilizzare il comando `bash` con il
+	// flag `-c` e passare la stringa del processo da eseguire.
+	lsCmd := exec.Command("bash", "-c", "ls -a -l -h")
+	lsOut, err := lsCmd.Output()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("> ls -a -l -h")
+	fmt.Println(string(lsOut))
 }
--- examples/spawning-processes/spawning-processes.sh
+++ examples/spawning-processes/spawning-processes.sh
@@ -1,5 +1,6 @@
-# The spawned programs return output that is the same
-# as if we had run them directly from the command-line.
+# Il processo eseguito stampa lo stesso output
+# che avrebbe stampato se lo avessimo eseguito
+# direttamente da terminale
 $ go run spawning-processes.go 
 > date
 Wed Oct 10 09:53:11 PDT 2012
