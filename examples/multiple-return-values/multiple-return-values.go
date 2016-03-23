c23ab64726640453fa834d0a8771e6de193c0836
Howl <git@howl.moe>
2016-03-23 20:24:30 +0100

    Apply fixes for 13: multiple return values

--- examples/multiple-return-values/multiple-return-values.go
+++ examples/multiple-return-values/multiple-return-values.go
@@ -1,4 +1,4 @@
-// Go supporta _multipli valori di ritorno_, similmente
+// Go supporta _valori di ritorno multipli_, similmente
 // a python. Questa funzionalità è usata spesso nel Go
 // idiomatico, per esempio per ritornare sia il valore
 // sia l'eventuale errore nell'esecuzione di una funzione.
@@ -7,8 +7,8 @@ package main
 
 import "fmt"
 
-// L'`(int, int)` in questa funzione ci dice che la
-// funzione ritorna due `int`.
+// L'indicazione `(int, int)` in questa funzione ci dice
+// che la funzione ritorna due `int`.
 func vals() (int, int) {
 	return 3, 7
 }
@@ -18,8 +18,6 @@ func main() {
 	// Di seguito utilizzando il _multiple assignment_
 	// creiamo due diverse variabili dai valori di ritorno
 	// della funzione `vals()`.
-	// Here we use the 2 different return values from the
-	// call with _multiple assignment_.
 	a, b := vals()
 	fmt.Println(a)
 	fmt.Println(b)
