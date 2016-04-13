fdd6ea60249f2360ba954a1c57a37f68f907a27f
Nicola Corti <corti.nico@gmail.com>
2016-04-13 23:00:03 +0200

    Fixed minor errors in channel buffering

--- examples/channel-buffering/channel-buffering.go
+++ examples/channel-buffering/channel-buffering.go
@@ -1,6 +1,6 @@
-// Per default i channels vengono creati senza _buffer_. 
+// Per default i channel vengono creati senza _buffer_.
 // Scrivere un valore in un channel (`chan <-`) sarà quindi bloccante
-// non c'è nessuno in ascolto su quel channel (`<- chan`).
+// non c'è nessun ricevitore in ascolto su quel channel (`<- chan`).
 // I channel con _buffer_ accettano un numero limitato di valori e non
 // risultano essere bloccanti se nessuno sta ascoltando sul
 // channel in questione
@@ -11,16 +11,16 @@ import "fmt"
 
 func main() {
 
-    // Qui creiamo un channel (con `make`) di string con un 
-    // buffer di dimensione 2.
-    messages := make(chan string, 2)
+	// Qui creiamo un channel di string (con `make`) con un
+	// buffer di dimensione 2.
+	messages := make(chan string, 2)
 
-    // Dato che questo channel è bufferizzato, possiamo inviare questi 
-    // due valori sul canale senza bloccarsi.
-    messages <- "buffered"
-    messages <- "channel"
+	// Dato che questo channel è bufferizzato, possiamo inviare questi
+	// due valori sul channel senza che il processo si blocchi.
+	messages <- "buffered"
+	messages <- "channel"
 
-    // Possiamo recuperare successivamente i valori lasciati sul canale.
-    fmt.Println(<-messages)
-    fmt.Println(<-messages)
+	// Possiamo recuperare successivamente i valori ricevuti sul channel.
+	fmt.Println(<-messages)
+	fmt.Println(<-messages)
 }
