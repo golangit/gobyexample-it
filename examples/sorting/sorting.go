7961201face50bb7139ce31d1a02d4c737c321cf
Howl <the@howl.moe>
2016-04-17 19:57:59 +0200

    Localisation for example 39: sorting

--- examples/sorting/sorting.go
+++ examples/sorting/sorting.go
@@ -1,6 +1,6 @@
-// Go's `sort` package implements sorting for builtins
-// and user-defined types. We'll look at sorting for
-// builtins first.
+// Il package `sort` di Go permette di ordinare tipi
+// sia builtin, sia user-defined. Daremo per prima cosa
+// un'occhiata al sorting per i builtin.
 
 package main
 
@@ -9,21 +9,22 @@ import "sort"
 
 func main() {
 
-    // Sort methods are specific to the builtin type;
-    // here's an example for strings. Note that sorting is
-    // in-place, so it changes the given slice and doesn't
-    // return a new one.
-    strs := []string{"c", "a", "b"}
-    sort.Strings(strs)
-    fmt.Println("Strings:", strs)
+	// I metodi di sort sono specifici per il tipo
+	// builtin; qui vediamo un esempio per le string.
+	// Nota che il sorting non richiede né l'uso dei
+	// pointer, né ritorna un nuovo slice con gli
+	// elementi ordinati.
+	strs := []string{"c", "a", "b"}
+	sort.Strings(strs)
+	fmt.Println("Strings:", strs)
 
-    // An example of sorting `int`s.
-    ints := []int{7, 2, 4}
-    sort.Ints(ints)
-    fmt.Println("Ints:   ", ints)
+	// Un esempio del sorting di `int`.
+	ints := []int{7, 2, 4}
+	sort.Ints(ints)
+	fmt.Println("Ints:   ", ints)
 
-    // We can also use `sort` to check if a slice is
-    // already in sorted order.
-    s := sort.IntsAreSorted(ints)
-    fmt.Println("Sorted: ", s)
+	// Possiamo anche usare `sort` per determinare
+	// se uno slice è già stato ordinato.
+	s := sort.IntsAreSorted(ints)
+	fmt.Println("Sorted: ", s)
 }
--- examples/sorting/sorting.sh
+++ examples/sorting/sorting.sh
@@ -1,5 +1,6 @@
-# Running our program prints the sorted string and int
-# slices and `true` as the result of our `AreSorted` test.
+# Eseguendo il nostro programma verrano stampati gli
+# slice di stringhe e di int, e `true` come risultato
+# della nostra chiamata alla funzione `AreSorted`.
 $ go run sorting.go
 Strings: [a b c]
 Ints:    [2 4 7]
