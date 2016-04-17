fa0e767e9ad9a02d31912dba43fa158ea1cdd52d
Howl <the@howl.moe>
2016-04-17 16:07:35 +0200

    Apply fixes for 31: range over channels

--- examples/range-over-channels/range-over-channels.go
+++ examples/range-over-channels/range-over-channels.go
@@ -1,7 +1,7 @@
 // In un [esempio precedente](range), abbiamo visto come
-// `for` e `range` ci diano la possibilità di reiterare
+// `for` e `range` ci diano la possibilità di iterare
 // su strutture di dati semplici. Possiamo usare la stessa
-// sintassi anche per reiterare sui valori ricevuti da un
+// sintassi anche per iterare sui valori ricevuti da un
 // channel.
 
 package main
@@ -10,7 +10,7 @@ import "fmt"
 
 func main() {
 
-	// Reitereremo su 2 valori nel canale `queue`.
+	// Itereremo su 2 valori nel canale `queue`.
 	queue := make(chan string, 2)
 	queue <- "one"
 	queue <- "two"
@@ -19,10 +19,10 @@ func main() {
 	// Questo `range` reitera su ogni elemento ogni qual
 	// volta che viene ricevuto dal channel `queue`.
 	// Poiché abbiamo effettuato un `close` sul canale
-	// in precedenza, la reiterazione terminerà dopo aver
+	// in precedenza, la iterazione terminerà dopo aver
 	// ricevuto i 2 elementi. Se non avessimo effettuato
 	// il `close` sul canale, allora saremmo bloccati ad
-	// attendere per un terzo ricevimento nel ciclo.
+	// attendere per un terzo futuro valore
 	for elem := range queue {
 		fmt.Println(elem)
 	}
--- examples/range-over-channels/range-over-channels.sh
+++ examples/range-over-channels/range-over-channels.sh
@@ -2,6 +2,6 @@ $ go run range-over-channels.go
 one
 two
 
-# Questo esempio ci ha inoltre dimostrato che è comunque
-# possibile chiudere un canale non vuoto, ma ricevere
-# comunque i valori rimanenti.
+# Questo esempio ci ha dimostrato che è comunque
+# possibile chiudere un canale non vuoto e ricevere
+# i valori rimanenti.
